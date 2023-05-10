79769643377fab963d92b9d2e7768840569778c1
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-10 20:21:34 +0300

    add bg images anf svg

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,11 @@
 import logo from "./logo/logo.jpg";
+import heroBgDesc from "./background/heroDesk.webp";
+import heroBgDesc2x from "./background/heroDesk2x.webp";
 
-export const IMG = { logo };
+import { ReactComponent as UserCheck } from "./svg/user-check.svg";
+import { ReactComponent as Calendar } from "./svg/calendar.svg";
+import { ReactComponent as BookOpen } from "./svg/book-open.svg";
+
+export const IMG = { logo, heroBgDesc, heroBgDesc2x };
+
+export const SVG = { UserCheck, Calendar, BookOpen };
