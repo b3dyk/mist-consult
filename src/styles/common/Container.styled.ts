83e0898c136d773bcaa97b267c252da45463ac49
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-10 17:40:15 +0300

    add Container

---
+++ src/styles/common/Container.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    padding: 0 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+  }
+`;
