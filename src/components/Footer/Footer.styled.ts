32ea89596e9ed76cd3a40de3e5632f97db0467ae
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-13 16:00:10 +0300

    add footer

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+export const Footer = styled.footer`
+  padding-top: 50px;
+  padding-bottom: 50px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Text = styled.p`
+  font-weight: ${(p) => p.theme.fontWeight.medium};
+  font-size: ${(p) => p.theme.fontSize.m};
+  line-height: 1.5;
+`;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,15 @@
+import { Container } from "../../styles/common";
+import * as SC from "./Footer.styled";
+
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <SC.Footer>
+      <Container>
+        <SC.Wrapper>
+          <SC.Text>&#169; Mist Consult 2023. All rights reserved</SC.Text>
+          <SC.Text>{`+38 (097) 555 55 55`}</SC.Text>
+        </SC.Wrapper>
+      </Container>
+    </SC.Footer>
+  );
 };
