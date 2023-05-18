17f8c84b9af2922167be72322ece603a27dc6d02
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-18 21:20:01 +0300

    add images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,31 +1,81 @@
 import logo from "./logo/logo.jpg";
+import heroBgMob from "./background/heroMob.webp";
+import heroBgMob2x from "./background/heroMob2x.webp";
+import heroBgTab from "./background/heroTab.webp";
+import heroBgTab2x from "./background/heroTab2x.webp";
 import heroBgDesc from "./background/heroDesk.webp";
 import heroBgDesc2x from "./background/heroDesk2x.webp";
-import aboutUs from "./about/about-us.webp";
-import aboutUs2x from "./about/about-us2x.webp";
-import serviceCard1 from "./services/field.webp";
-import serviceCard12x from "./services/field2x.webp";
-import serviceCard2 from "./services/building.webp";
-import serviceCard22x from "./services/building2x.webp";
-import serviceCard3 from "./services/street.webp";
-import serviceCard32x from "./services/street2x.webp";
+import aboutUs from "./about/about-us-img.jpg";
+import aboutUsMob from "./about/about-us-mob.webp";
+import aboutUsMob2x from "./about/about-us-mob2x.webp";
+import aboutUsTab from "./about/about-us-tab.webp";
+import aboutUsTab2x from "./about/about-us-tab2x.webp";
+import aboutUsDesk from "./about/about-us.webp";
+import aboutUsDesk2x from "./about/about-us2x.webp";
+import serviceCard1 from "./services/field.jpg";
+import serviceCard1Mob from "./services/fieldMob.webp";
+import serviceCard1Mob2x from "./services/fieldMob2x.webp";
+import serviceCard1Tab from "./services/fieldTab.webp";
+import serviceCard1Tab2x from "./services/fieldTab2x.webp";
+import serviceCard1Desk from "./services/field.webp";
+import serviceCard1Desk2x from "./services/field2x.webp";
+import serviceCard2 from "./services/building.jpg";
+import serviceCard2Mob from "./services/buildingMob.webp";
+import serviceCard2Mob2x from "./services/buildingMob2x.webp";
+import serviceCard2Tab from "./services/buildingTab.webp";
+import serviceCard2Tab2x from "./services/buildingTab2x.webp";
+import serviceCard2Desk from "./services/building.webp";
+import serviceCard2Desk2x from "./services/building2x.webp";
+import serviceCard3 from "./services/street.jpg";
+import serviceCard3Mob from "./services/streetMob.webp";
+import serviceCard3Mob2x from "./services/streetMob2x.webp";
+import serviceCard3Tab from "./services/streetTab.webp";
+import serviceCard3Tab2x from "./services/streetTab2x.webp";
+import serviceCard3Desk from "./services/street.webp";
+import serviceCard3Desk2x from "./services/street2x.webp";
 
 import { ReactComponent as UserCheck } from "./svg/user-check.svg";
 import { ReactComponent as Calendar } from "./svg/calendar.svg";
 import { ReactComponent as BookOpen } from "./svg/book-open.svg";
+import { ReactComponent as Burger } from "./svg/burger.svg";
+import { ReactComponent as Close } from "./svg/close.svg";
 
 export const IMG = {
   logo,
+  heroBgMob,
+  heroBgMob2x,
+  heroBgTab,
+  heroBgTab2x,
   heroBgDesc,
   heroBgDesc2x,
   aboutUs,
-  aboutUs2x,
+  aboutUsMob,
+  aboutUsMob2x,
+  aboutUsTab,
+  aboutUsTab2x,
+  aboutUsDesk,
+  aboutUsDesk2x,
   serviceCard1,
-  serviceCard12x,
+  serviceCard1Mob,
+  serviceCard1Tab,
+  serviceCard1Desk,
+  serviceCard1Mob2x,
+  serviceCard1Tab2x,
+  serviceCard1Desk2x,
   serviceCard2,
-  serviceCard22x,
+  serviceCard2Mob,
+  serviceCard2Tab,
+  serviceCard2Desk,
+  serviceCard2Mob2x,
+  serviceCard2Tab2x,
+  serviceCard2Desk2x,
   serviceCard3,
-  serviceCard32x,
+  serviceCard3Mob,
+  serviceCard3Tab,
+  serviceCard3Desk,
+  serviceCard3Mob2x,
+  serviceCard3Tab2x,
+  serviceCard3Desk2x,
 };
 
-export const SVG = { UserCheck, Calendar, BookOpen };
+export const SVG = { UserCheck, Calendar, BookOpen, Burger, Close };
