921777fdf6ae14c8a7189ca78b1d02c67c13dbaf
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-18 21:21:35 +0300

    Update AboutSection.styled.ts

--- src/pages/HomePage/AboutSection/AboutSection.styled.ts
+++ src/pages/HomePage/AboutSection/AboutSection.styled.ts
@@ -1,33 +1,65 @@
 import styled from "styled-components";
 
 export const AboutSection = styled.section`
-  padding-top: 80px;
-  padding-bottom: 80px;
+  padding-top: 20px;
+  padding-bottom: 20px;
   background-color: ${(p) => p.theme.color.bgGrey};
+
+  @media screen and (min-width: 768px) {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 80px;
+    padding-bottom: 80px;
+  }
 `;
 
 export const Thumb = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 64px;
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 64px;
+  }
 `;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  border-radius: ${(p) => p.theme.radii.normal};
+
+  @media screen and (max-width: 1439px) {
+    margin-bottom: 24px;
+  }
+`;
 
 export const Wrapper = styled.div`
-  flex-basis: 49%;
+  @media screen and (min-width: 1440px) {
+    flex-basis: calc((100% - 64px * (2 - 1)) / 2);
+  }
 `;
 
 export const Title = styled.h2`
-  margin-bottom: 24px;
   font-weight: ${(p) => p.theme.fontWeight.bold};
-  font-size: ${(p) => p.theme.fontSize.bxl};
-  line-height: 1.219;
+  font-size: ${(p) => p.theme.fontSize.xxxl};
+  line-height: 1.208;
+  margin-bottom: 24px;
+  color: ${(p) => p.theme.color.textMain};
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.bxl};
+    line-height: 1.219;
+  }
 `;
 
 export const Description = styled.p`
   font-weight: ${(p) => p.theme.fontWeight.regular};
-  font-size: ${(p) => p.theme.fontSize.xxxl};
+  font-size: ${(p) => p.theme.fontSize.xl};
   line-height: 1.5;
+  color: ${(p) => p.theme.color.textMain};
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.xxxl};
+    line-height: 1.5;
+  }
 `;
