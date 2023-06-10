1fb9fa4b91ef1118e6cdd43a27041976efa2b9cc
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-06-10 16:28:45 +0300

    Update ServicesSection.styled.ts

--- src/pages/HomePage/ServicesSection/ServicesSection.styled.ts
+++ src/pages/HomePage/ServicesSection/ServicesSection.styled.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const ServicesSection = styled.section`
   padding-top: 40px;
   padding-bottom: 40px;
-  /* text-align: center; */
+  text-align: center;
 
   @media screen and (min-width: 1440px) {
     padding-top: 80px;
@@ -18,7 +18,6 @@ export const Title = styled.h2`
   font-size: ${(p) => p.theme.fontSize.bxl};
   line-height: 1.219;
   color: ${(p) => p.theme.color.textMain};
-  text-align: center;
 `;
 
 export const List = styled.ul`
@@ -28,7 +27,7 @@ export const List = styled.ul`
   gap: 20px;
   margin-bottom: 40px;
 
-  @media screen and (max-width: 767px) {
+  @media screen and (max-width: 1439px) {
     flex-direction: column;
   }
 
@@ -43,9 +42,11 @@ export const CardItem = styled.li`
   border: 1px solid ${(p) => p.theme.color.accent};
   border-radius: ${(p) => p.theme.radii.normal};
   overflow: hidden;
+  width: 100%;
+  padding-bottom: 24px;
+  height: 320px;
 
   @media screen and (min-width: 1440px) {
-    padding-bottom: 24px;
     transition: transform ${(p) => p.theme.animation.main};
 
     :hover {
@@ -60,7 +61,7 @@ export const CardLink = styled(Link)`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  min-height: 320px;
+  height: 100%;
 `;
 
 export const CardTitle = styled.span`
