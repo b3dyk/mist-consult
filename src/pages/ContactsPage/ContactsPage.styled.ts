4531b61e1d799ffc39bc2e1c7efbaf47ce3484e5
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-06-10 21:36:16 +0300

    add Contacts Page

---
+++ src/pages/ContactsPage/ContactsPage.styled.ts
@@ -0,0 +1,287 @@
+import styled from "styled-components";
+import { SVG } from "../../assets/images";
+
+export const ContactsSection = styled.section`
+  padding-top: 58px;
+  padding-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 80px;
+    padding-bottom: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 80px;
+    padding-bottom: 40px;
+  }
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 40px;
+  font-weight: ${(p) => p.theme.fontWeight.bold};
+  font-size: ${(p) => p.theme.fontSize.xxxl};
+  line-height: 1.219;
+
+  color: ${(p) => p.theme.color.textMain};
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.bxl};
+    margin-bottom: 64px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    margin-bottom: 80px;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  position: absolute;
+  top: 77px;
+  left: 50%;
+  z-index: -1;
+  transform: translateX(-50%);
+  font-weight: ${(p) => p.theme.fontWeight.bold};
+  font-size: 64px;
+  line-height: 1.219;
+
+  color: ${(p) => p.theme.color.snowWhite};
+  text-shadow: 1px 0px 1px rgba(0, 0, 0, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.1),
+    -1px 0px 1px rgba(0, 0, 0, 0.1), 0px -1px 1px rgba(0, 0, 0, 0.1);
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 100px;
+    line-height: 1.22;
+    top: 144px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    font-size: 150px;
+    top: 106px;
+  }
+`;
+
+export const Logo = styled(SVG.Logo)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: -1;
+  transform: translateX(-50%);
+  opacity: 0.1;
+  max-width: 500px;
+  width: 100%;
+  height: 500px;
+
+  fill: ${(p) => p.theme.color.accent};
+  filter: blur(2.5px);
+
+  @media screen and (min-width: 768px) {
+    max-width: 800px;
+    height: 800px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    top: 45%;
+    max-width: 1000px;
+    height: 1000px;
+  }
+`;
+
+export const ContactsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: row;
+  }
+`;
+
+export const ContactItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  border: ${(p) => p.theme.border.regular};
+  border-radius: ${(p) => p.theme.radii.normal};
+  background-color: ${(p) => p.theme.color.bgGrey};
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-right: 16px;
+  padding-left: 16px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 32px;
+    padding-bottom: 32px;
+    padding-right: 24px;
+    padding-left: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex-basis: calc((100% - 40px) / 3);
+    :not(:last-of-type) {
+      gap: 42px;
+    }
+  }
+`;
+
+export const ContactTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const Phone = styled(SVG.Phone)`
+  width: 24px;
+  height: 24px;
+`;
+export const Envelope = styled(SVG.Envelope)`
+  width: 26px;
+  height: 21px;
+`;
+export const Pin = styled(SVG.Pin)`
+  width: 26px;
+  height: 30px;
+`;
+
+export const ContsctTitle = styled.h3`
+  font-weight: ${(p) => p.theme.fontWeight.semiBold};
+  font-size: ${(p) => p.theme.fontSize.xl};
+  line-height: 1.2;
+  color: ${(p) => p.theme.color.textMain};
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.xxxl};
+  }
+`;
+
+export const Contact = styled.p`
+  font-weight: ${(p) => p.theme.fontWeight.medium};
+  font-size: ${(p) => p.theme.fontSize.m};
+  line-height: 1.5;
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.xl};
+  }
+`;
+
+export const MapSection = styled.section`
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 32px;
+    padding-bottom: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+`;
+
+export const MapTitle = styled.h3`
+  font-weight: ${(p) => p.theme.fontWeight.semiBold};
+  font-size: ${(p) => p.theme.fontSize.xxxl};
+  line-height: 1.208;
+  color: ${(p) => p.theme.color.accent};
+  text-align: center;
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.bxl};
+    line-height: 1.219;
+    margin-bottom: 40px;
+  }
+`;
+
+export const Map = styled.iframe`
+  width: 100%;
+  height: 432px;
+  border: ${(p) => p.theme.border.regular};
+  border-radius: ${(p) => p.theme.radii.normal};
+`;
+
+export const FormSection = styled.section`
+  padding-top: 20px;
+  padding-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 32px;
+    padding-bottom: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+`;
+
+export const FormWrapper = styled.div`
+  position: relative;
+  border: ${(p) => p.theme.border.regular};
+  border-radius: ${(p) => p.theme.radii.normal};
+  padding: 40px 24px;
+  text-align: center;
+  background-color: ${(p) => p.theme.color.bgGrey};
+  overflow: hidden;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 120px;
+    padding-right: 120px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-left: 355px;
+    padding-right: 355px;
+  }
+`;
+
+export const EllipseTop = styled(SVG.EllipseTop)`
+  position: absolute;
+  top: -45px;
+  left: 0;
+
+  @media screen and (min-width: 768px) {
+    top: 0;
+  }
+`;
+
+export const EllipseBottom = styled(SVG.EllipseBottom)`
+  position: absolute;
+  bottom: -73px;
+  right: 0;
+
+  @media screen and (min-width: 768px) {
+    bottom: 0;
+  }
+`;
+
+export const FormTitle = styled.h2`
+  font-weight: ${(p) => p.theme.fontWeight.bold};
+  font-size: ${(p) => p.theme.fontSize.xxxl};
+  line-height: 1.219;
+  color: ${(p) => p.theme.color.accent};
+  margin-bottom: 8px;
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.bxl};
+    line-height: 1.219;
+  }
+`;
+
+export const FormText = styled.p`
+  font-weight: ${(p) => p.theme.fontWeight.medium};
+  font-size: ${(p) => p.theme.fontSize.m};
+  line-height: 1.208;
+  max-width: 425px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 48px;
+
+  @media screen and (min-width: 768px) {
+    font-size: ${(p) => p.theme.fontSize.xxxl};
+    line-height: 1.208;
+  }
+`;
--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,5 +1,71 @@
+import Form from "../../components/Form";
+import { Container } from "../../styles/common";
+import * as SC from "./ContactsPage.styled";
+import { useEffect } from "react";
+
 const ContactsPage: React.FC = () => {
-  return <></>;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <SC.Logo />
+        <SC.ContactsSection>
+          <SC.Subtitle>Контакти</SC.Subtitle>
+          <SC.Title>Контакти</SC.Title>
+
+          <SC.ContactsList>
+            <SC.ContactItem>
+              <SC.ContactTitleWrapper>
+                <SC.Phone />
+                <SC.ContsctTitle>Телефон</SC.ContsctTitle>
+              </SC.ContactTitleWrapper>
+              <SC.Contact>+38 095 140 14 40</SC.Contact>
+            </SC.ContactItem>
+            <SC.ContactItem>
+              <SC.ContactTitleWrapper>
+                <SC.Envelope />
+                <SC.ContsctTitle>Пошта</SC.ContsctTitle>
+              </SC.ContactTitleWrapper>
+              <SC.Contact>mist.consult.law@gmail.com</SC.Contact>
+            </SC.ContactItem>
+            <SC.ContactItem>
+              <SC.ContactTitleWrapper>
+                <SC.Pin />
+                <SC.ContsctTitle>Адреса</SC.ContsctTitle>
+              </SC.ContactTitleWrapper>
+              <SC.Contact>
+                м. Київ вул. Михайла Омеляновича-Павленка, 13
+              </SC.Contact>
+            </SC.ContactItem>
+          </SC.ContactsList>
+        </SC.ContactsSection>
+
+        <SC.MapSection>
+          <SC.MapTitle>Знайдіть нас на мапі</SC.MapTitle>
+          <SC.Map
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.255158137981!2d30.54473497697416!3d50.43634778837865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cfa7725cadcf%3A0x4411a7e67bbc2782!2z0LLRg9C70LjRhtGPINCc0LjRhdCw0LnQu9CwINCe0LzQtdC70Y_QvdC-0LLQuNGH0LAt0J_QsNCy0LvQtdC90LrQsCwgMTMsINCa0LjRl9CyLCAwMjAwMA!5e0!3m2!1suk!2sua!4v1686406007329!5m2!1suk!2sua"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></SC.Map>
+        </SC.MapSection>
+
+        <SC.FormSection>
+          <SC.FormWrapper>
+            <SC.EllipseTop />
+            <SC.EllipseBottom />
+            <SC.FormTitle>Зацікавили послуги?</SC.FormTitle>
+            <SC.FormText>
+              Введіть ваші контактні дані та ми вам зателефонуєм
+            </SC.FormText>
+            <Form modal={false} />
+          </SC.FormWrapper>
+        </SC.FormSection>
+      </Container>
+    </>
+  );
 };
 
 export default ContactsPage;
