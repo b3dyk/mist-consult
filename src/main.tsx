7fa27ecdd8b9098d5fe72829eefec3b10937c5b2
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-06-11 22:22:52 +0300

    add toastify styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { globalStyles } from "./styles/theme/theme";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
