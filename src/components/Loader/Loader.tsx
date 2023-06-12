72df13c65594eaab07007f77d88b4b5d74d595a1
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-06-12 21:02:54 +0300

    improve loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,19 +1,23 @@
 import { ThreeCircles } from "react-loader-spinner";
 
+import * as SC from "./Loader.styled";
+
 const Loader = () => {
   return (
-    <ThreeCircles
-      height="100"
-      width="100"
-      color="#29974e"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="three-circles-rotating"
-      outerCircleColor=""
-      innerCircleColor=""
-      middleCircleColor=""
-    />
+    <SC.Wrapper>
+      <ThreeCircles
+        height="100"
+        width="100"
+        color="#29974e"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor=""
+        innerCircleColor=""
+        middleCircleColor=""
+      />
+    </SC.Wrapper>
   );
 };
 
