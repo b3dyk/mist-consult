94e1ba4ae4d767bb3b16a67baf4d38181d49819b
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-06-24 12:29:25 +0300

    Update useModalHandler.ts

--- src/hooks/useModalHandler.ts
+++ src/hooks/useModalHandler.ts
@@ -2,30 +2,26 @@
 // import { useScrollLock } from "./useScrollLock";
 // import { useModal } from "./useModal";
 
-// export const useModalHandler = () => {
-//   const { toggleModal } = useModal();
-//   const { lockScroll, unlockScroll } = useScrollLock();
-
-//   useEffect(() => {
-//     const closeModal = (evt: Event): void => {
-//       if (evt instanceof KeyboardEvent && evt.code === "Escape") {
-//         toggleModal();
-//       }
-//     };
-//     window.addEventListener("keydown", closeModal);
-//     lockScroll();
-
-//     return () => {
-//       window.removeEventListener("keydown", closeModal);
-//       unlockScroll();
-//     };
-//   }, [toggleModal]);
-
-//   const closeOnClick = (evt: MouseEvent<HTMLDivElement>) => {
-//     if (evt.target === evt.currentTarget) {
-//       toggleModal();
-//     }
-//   };
-
-//   return { toggleModal, closeOnClick };
-// };
+export const useModalHandler = () => {
+  //   const { toggleModal } = useModal();
+  //   const { lockScroll, unlockScroll } = useScrollLock();
+  //   useEffect(() => {
+  //     const closeModal = (evt: Event): void => {
+  //       if (evt instanceof KeyboardEvent && evt.code === "Escape") {
+  //         toggleModal();
+  //       }
+  //     };
+  //     window.addEventListener("keydown", closeModal);
+  //     lockScroll();
+  //     return () => {
+  //       window.removeEventListener("keydown", closeModal);
+  //       unlockScroll();
+  //     };
+  //   }, [toggleModal]);
+  //   const closeOnClick = (evt: MouseEvent<HTMLDivElement>) => {
+  //     if (evt.target === evt.currentTarget) {
+  //       toggleModal();
+  //     }
+  //   };
+  //   return { toggleModal, closeOnClick };
+};
