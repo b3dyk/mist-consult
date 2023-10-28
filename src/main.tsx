3dcd9decdf6e51015825ef9a0e46d39b0f9c6d2a
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-10-28 14:35:25 +0300

    fix github 404 problem

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="mist-consult">
+    <BrowserRouter basename="/mist-consult">
       <ThemeProvider theme={globalStyles}>
         <App />
       </ThemeProvider>
